5cc6f5b20f95f5b36a233d91263aac575288f706
Julián Arias Maetschl <maetschl@gmail.com>
2017-04-21 22:47:34 -0300

    PlatformsTest
    StackOverflow:
    http://stackoverflow.com/questions/43544699/turning-physics-on-and-off

---
+++ PlatformTest/PlatformTest/GameScene.swift
@@ -0,0 +1,45 @@
+//
+//  GameScene.swift
+//  PlatformTest
+//
+//  Created by Maetschl on 21-04-17.
+//  Copyright © 2017 Maetschl. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var up1 : SKSpriteNode!
+    private var down1 : SKSpriteNode!
+    private var down2 : SKSpriteNode!
+    private var player : SKSpriteNode!
+    private var down : Bool! = true
+    
+    override func didMove(to view: SKView) {
+        up1 = self.childNode(withName: "up1") as! SKSpriteNode
+        down1 = self.childNode(withName: "down1") as! SKSpriteNode
+        down2 = self.childNode(withName: "down2") as! SKSpriteNode
+        player = self.childNode(withName: "player") as! SKSpriteNode
+        up1.physicsBody?.categoryBitMask = 0b0001 // Mask for UoPlatforms
+        down1.physicsBody?.categoryBitMask = 0b0010 // Mask for downPlatforms
+        down2.physicsBody?.categoryBitMask = 0b0010 // Same mask
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        player.physicsBody?.collisionBitMask = 0b0000 // Reset the mask
+        
+        // For UP only Platform
+        if (player.physicsBody?.velocity.dy)! < CGFloat(0.0) {
+            player.physicsBody?.collisionBitMask |= 0b0001 // The pipe | operator adds the mask by binary operations
+        }
+
+        // For Down only platforms
+        if (player.physicsBody?.velocity.dy)! > CGFloat(0.0) {
+            player.physicsBody?.collisionBitMask |= 0b0010  // The pipe | operator adds the mask by binary operations
+        }
+        
+    }
+    
+}
