3a318d01b298a7cdeadf05c3be00ef04b0b42bfb
Julián Arias Maetschl <maetschl@gmail.com>
2017-07-22 20:27:33 -0400

    Update
    https://stackoverflow.com/questions/45259118/scaling-a-pendulum-made-wit
    h-skspritenodes/45259526?noredirect=1#comment77483243_45259526

--- TestJoint/TestJoint/GameScene.swift
+++ TestJoint/TestJoint/GameScene.swift
@@ -21,10 +21,17 @@ class GameScene: SKScene {
         
         let node2Size = CGSize(width: 60, height: 8)
         let node2 = SKSpriteNode(color: .green, size: node2Size)
-        node2.position = CGPoint(x: 30, y: 0)
+        node2.position = CGPoint(x: 5, y: 0)
+        node2.anchorPoint = CGPoint(x: 0.0, y: 0.5) // <- New Line
         node2.physicsBody = SKPhysicsBody(rectangleOf: node2Size)
         node2.physicsBody?.mass = 1.0
         self.addChild(node2)
+        
+        // Scale Line
+        node2.run(SKAction.repeatForever(SKAction.sequence([
+            SKAction.scale(to: 0.2, duration: 1.0),
+            SKAction.scale(to: 1.5, duration: 0.5),
+            ])))
         
         
         let a = SKPhysicsJointPin.joint(withBodyA: node.physicsBody! , bodyB: node2.physicsBody!, anchor: CGPoint(x: 0.0, y: 0.0))
