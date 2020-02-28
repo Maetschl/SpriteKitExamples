ba1d2496611ddc84780fc97a33318246cda51d4e
Julián Arias Maetschl <maetschl@gmail.com>
2020-02-28 20:55:40 -0300

    Add Component example project

---
+++ ComponentTest/ComponentTest/NewGameComponent.swift
@@ -0,0 +1,13 @@
+//
+//  NewGameComponent.swift
+//  ComponentTest
+//
+//  Created by Julian Arias Maetschl on 28-02-20.
+//  Copyright © 2020 Maetschl. All rights reserved.
+//
+
+import GameplayKit
+
+class NewGameComponent: GKComponent {
+    
+}
