41840d8c34a53e19efe078087aec86d692b9c9de
Julián Arias Maetschl <maetschl@gmail.com>
2020-03-08 13:31:56 -0300

    Example for https://stackoverflow.com/questions/60579025/sktilemapnode-tilerowindex-always-returns-0-for-a-negative-value-outside-its-bou

---
+++ TileMapExamples/TileMapExamples/GameScene.swift
@@ -0,0 +1,40 @@
+//
+//  GameScene.swift
+//  TileMapExamples
+//
+//  Created by Julian Arias Maetschl on 08-03-20.
+//  Copyright © 2020 Maetschl. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+    private var tileMap : SKTileMapNode!
+    
+    override func didMove(to view: SKView) {
+        self.tileMap = childNode(withName: "tileMap") as? SKTileMapNode
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        var color: SKColor = .black
+        if tileMap.contains(pos) {
+            // red
+        } else {
+            color = .white
+        }
+        let node = SKSpriteNode(color: color, size: CGSize(width: 15, height: 15))
+        node.position = pos
+        addChild(node)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
