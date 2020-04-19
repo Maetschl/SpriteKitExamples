a75fe2e57d72209ed4dd8c234ac775ed888e29db
Julián Arias Maetschl <maetschl@gmail.com>
2020-04-19 00:28:43 -0400

    Circular Range example

---
+++ CircularRange/CircularRange/GameScene.swift
@@ -0,0 +1,38 @@
+//
+//  GameScene.swift
+//  CircularRange
+//
+//  Created by Julian Arias Maetschl on 19-04-20.
+//  Copyright © 2020 Maetschl. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+        for index in 0 ... 600 {
+            let node = SKSpriteNode(color: .white, size: CGSize(width: 3, height: 3))
+            let xRand = (-300 ... 300).randomElement()!
+            let yRand = (-300 ... 300).randomElement()!
+            node.color = .red
+            if index%2 == 0 {
+                node.color = .green
+                makeCircularRange(to: node)
+            }
+            node.run(SKAction.move(to: CGPoint(x: xRand, y: yRand), duration: 5))
+            addChild(node)
+        }
+
+    }
+
+    func makeCircularRange(to node: SKNode) {
+        let range = SKRange(lowerLimit: 0, upperLimit: 100)
+        let contraint = SKConstraint.distance(range, to: .zero)
+        node.constraints = [contraint]
+    }
+    
+
+}
