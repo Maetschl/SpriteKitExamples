1bb88d8f457532e4fd7ca074f482ce91f3cee7a2
Julián Arias Maetschl <maetschl@gmail.com>
2020-12-10 13:32:28 -0300

    Tabs

--- CurvedPhysicsBody/CurvedPhysicsBody/GameScene.swift
+++ CurvedPhysicsBody/CurvedPhysicsBody/GameScene.swift
@@ -14,11 +14,11 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-self.node = self.childNode(withName: "//helloLabel") as? SKLabelNode
-let path = CGMutablePath()
-path.addLine(to: CGPoint(x: 10, y: 10))
-path.addArc(center: CGPoint(x: 20, y: 20), radius: 50, startAngle: 20, endAngle: 60, clockwise: true)
-node?.physicsBody = SKPhysicsBody(edgeLoopFrom: path)
+        self.node = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        let path = CGMutablePath()
+        path.addLine(to: CGPoint(x: 10, y: 10))
+        path.addArc(center: CGPoint(x: 20, y: 20), radius: 50, startAngle: 20, endAngle: 60, clockwise: true)
+        node?.physicsBody = SKPhysicsBody(edgeLoopFrom: path)
     }
     
 }
